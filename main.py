f9bd6ce2289127c033d47b3d6e5b41d39781e1b0
Rohan Dhananjaya <rohandhananjaya@gmail.com>
2023-07-28 23:21:14 +0530

    innitail commit

---
+++ main.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+from tkinter import messagebox
+
+class SimplePayroll:
+    print("Hello World")
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = SimplePayroll(root)
+    root.mainloop()
